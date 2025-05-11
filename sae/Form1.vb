0fca5c90c7bb36ffc058e406bea01a177b57dd44
celia xu <celia.xu@etu.u-paris.fr>
2025-05-11 22:58:42 +0200

    ajout dans le Form D'accueil

--- sae/Form1.vb
+++ sae/Form1.vb
@@ -1,10 +1,38 @@
 ﻿Public Class Form1
+
+    Dim tnoms() As String = {"le russe", "paracétamol", "amoxiciline", "doliprane", "smecta", "protagoniste", "sarah", "fitia", "eva", "celia"}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer = 0
+        For Each s As String In tnoms
+            cbxNoms.Items.Add(tnoms(i))
+            i += 1
+        Next
+    End Sub
+
     Private Sub btnLancer_Click(sender As Object, e As EventArgs) Handles btnLancer.Click
-        If cbxNom.Text.Length >= 3 Then
-            Dim nom As String = cbxNom.Text
-            Memory.Show()
-            Memory.Label2.Text = nom
+        Dim nomValide As Boolean = False
+
+        If cbxNoms.Text.Length < 3 Then
+            Exit Sub
         End If
+
+        For Each s As String In tnoms
+            If s = cbxNoms.Text Then
+                nomValide = True
+                Exit For
+            End If
+        Next
+
+        If nomValide = False Then
+            cbxNoms.Focus()
+            Exit Sub
+        End If
+
+        Dim nom As String = cbxNoms.Text
+        Memory.Show()
+        Memory.Label2.Text = nom
+
     End Sub
 
     Private Sub btnQuitter_Click(sender As Object, e As EventArgs) Handles btnQuitter.Click
@@ -12,5 +40,7 @@
         If choix = vbYes Then End
     End Sub
 
-
+    Private Sub btnAfficherScore_Click(sender As Object, e As EventArgs) Handles btnAfficherScore.Click
+        FormScores.Show()
+    End Sub
 End Class
