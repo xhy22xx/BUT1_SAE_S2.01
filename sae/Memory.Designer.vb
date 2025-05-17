dc05f8a719f21fc1ef51bf5d383722afb91fa1ed
celia xu <celia.xu@etu.u-paris.fr>
2025-05-17 13:54:34 +0200

    Formulaire Memory: j'ai enlevé les bordures

--- sae/Memory.Designer.vb
+++ sae/Memory.Designer.vb
@@ -127,268 +127,307 @@ Partial Class Memory
         ' 
         ' PictureBox1
         ' 
+        PictureBox1.BackColor = Color.Transparent
         PictureBox1.BackgroundImageLayout = ImageLayout.None
+        PictureBox1.Cursor = Cursors.Hand
         PictureBox1.Dock = DockStyle.Fill
         PictureBox1.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox1.Location = New Point(5, 6)
+        PictureBox1.Location = New Point(3, 4)
         PictureBox1.Margin = New Padding(3, 4, 3, 4)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(221, 201)
+        PictureBox1.Size = New Size(224, 204)
         PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox1.TabIndex = 0
         PictureBox1.TabStop = False
         ' 
         ' PictureBox2
         ' 
+        PictureBox2.BackColor = Color.Transparent
         PictureBox2.BackgroundImageLayout = ImageLayout.None
+        PictureBox2.Cursor = Cursors.Hand
         PictureBox2.Dock = DockStyle.Fill
         PictureBox2.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox2.Location = New Point(234, 6)
+        PictureBox2.Location = New Point(233, 4)
         PictureBox2.Margin = New Padding(3, 4, 3, 4)
         PictureBox2.Name = "PictureBox2"
-        PictureBox2.Size = New Size(221, 201)
+        PictureBox2.Size = New Size(224, 204)
         PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox2.TabIndex = 1
         PictureBox2.TabStop = False
         ' 
         ' PictureBox3
         ' 
+        PictureBox3.BackColor = Color.Transparent
         PictureBox3.BackgroundImageLayout = ImageLayout.None
+        PictureBox3.Cursor = Cursors.Hand
         PictureBox3.Dock = DockStyle.Fill
         PictureBox3.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox3.Location = New Point(463, 6)
+        PictureBox3.Location = New Point(463, 4)
         PictureBox3.Margin = New Padding(3, 4, 3, 4)
         PictureBox3.Name = "PictureBox3"
-        PictureBox3.Size = New Size(221, 201)
+        PictureBox3.Size = New Size(224, 204)
         PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox3.TabIndex = 2
         PictureBox3.TabStop = False
         ' 
         ' PictureBox4
         ' 
+        PictureBox4.BackColor = Color.Transparent
         PictureBox4.BackgroundImageLayout = ImageLayout.None
+        PictureBox4.Cursor = Cursors.Hand
         PictureBox4.Dock = DockStyle.Fill
         PictureBox4.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox4.Location = New Point(692, 6)
+        PictureBox4.Location = New Point(693, 4)
         PictureBox4.Margin = New Padding(3, 4, 3, 4)
         PictureBox4.Name = "PictureBox4"
-        PictureBox4.Size = New Size(223, 201)
+        PictureBox4.Size = New Size(224, 204)
         PictureBox4.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox4.TabIndex = 3
         PictureBox4.TabStop = False
         ' 
         ' PictureBox5
         ' 
+        PictureBox5.BackColor = Color.Transparent
         PictureBox5.BackgroundImageLayout = ImageLayout.None
+        PictureBox5.Cursor = Cursors.Hand
         PictureBox5.Dock = DockStyle.Fill
         PictureBox5.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox5.Location = New Point(5, 217)
+        PictureBox5.Location = New Point(3, 216)
         PictureBox5.Margin = New Padding(3, 4, 3, 4)
         PictureBox5.Name = "PictureBox5"
-        PictureBox5.Size = New Size(221, 201)
+        PictureBox5.Size = New Size(224, 204)
         PictureBox5.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox5.TabIndex = 4
         PictureBox5.TabStop = False
         ' 
         ' PictureBox6
         ' 
+        PictureBox6.BackColor = Color.Transparent
         PictureBox6.BackgroundImageLayout = ImageLayout.None
+        PictureBox6.Cursor = Cursors.Hand
         PictureBox6.Dock = DockStyle.Fill
         PictureBox6.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox6.Location = New Point(234, 217)
+        PictureBox6.Location = New Point(233, 216)
         PictureBox6.Margin = New Padding(3, 4, 3, 4)
         PictureBox6.Name = "PictureBox6"
-        PictureBox6.Size = New Size(221, 201)
+        PictureBox6.Size = New Size(224, 204)
         PictureBox6.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox6.TabIndex = 5
         PictureBox6.TabStop = False
         ' 
         ' PictureBox7
         ' 
+        PictureBox7.BackColor = Color.Transparent
         PictureBox7.BackgroundImageLayout = ImageLayout.None
+        PictureBox7.Cursor = Cursors.Hand
         PictureBox7.Dock = DockStyle.Fill
         PictureBox7.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox7.Location = New Point(463, 217)
+        PictureBox7.Location = New Point(463, 216)
         PictureBox7.Margin = New Padding(3, 4, 3, 4)
         PictureBox7.Name = "PictureBox7"
-        PictureBox7.Size = New Size(221, 201)
+        PictureBox7.Size = New Size(224, 204)
         PictureBox7.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox7.TabIndex = 6
         PictureBox7.TabStop = False
         ' 
         ' PictureBox8
         ' 
+        PictureBox8.BackColor = Color.Transparent
         PictureBox8.BackgroundImageLayout = ImageLayout.None
+        PictureBox8.Cursor = Cursors.Hand
         PictureBox8.Dock = DockStyle.Fill
         PictureBox8.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox8.Location = New Point(692, 217)
+        PictureBox8.Location = New Point(693, 216)
         PictureBox8.Margin = New Padding(3, 4, 3, 4)
         PictureBox8.Name = "PictureBox8"
-        PictureBox8.Size = New Size(223, 201)
+        PictureBox8.Size = New Size(224, 204)
         PictureBox8.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox8.TabIndex = 7
         PictureBox8.TabStop = False
         ' 
         ' PictureBox9
         ' 
+        PictureBox9.BackColor = Color.Transparent
         PictureBox9.BackgroundImageLayout = ImageLayout.None
+        PictureBox9.Cursor = Cursors.Hand
         PictureBox9.Dock = DockStyle.Fill
         PictureBox9.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox9.Location = New Point(5, 428)
+        PictureBox9.Location = New Point(3, 428)
         PictureBox9.Margin = New Padding(3, 4, 3, 4)
         PictureBox9.Name = "PictureBox9"
-        PictureBox9.Size = New Size(221, 201)
+        PictureBox9.Size = New Size(224, 204)
         PictureBox9.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox9.TabIndex = 8
         PictureBox9.TabStop = False
         ' 
         ' PictureBox10
         ' 
+        PictureBox10.BackColor = Color.Transparent
         PictureBox10.BackgroundImageLayout = ImageLayout.None
+        PictureBox10.Cursor = Cursors.Hand
         PictureBox10.Dock = DockStyle.Fill
         PictureBox10.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox10.Location = New Point(234, 428)
+        PictureBox10.Location = New Point(233, 428)
         PictureBox10.Margin = New Padding(3, 4, 3, 4)
         PictureBox10.Name = "PictureBox10"
-        PictureBox10.Size = New Size(221, 201)
+        PictureBox10.Size = New Size(224, 204)
         PictureBox10.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox10.TabIndex = 9
         PictureBox10.TabStop = False
         ' 
         ' PictureBox11
         ' 
+        PictureBox11.BackColor = Color.Transparent
         PictureBox11.BackgroundImageLayout = ImageLayout.None
+        PictureBox11.Cursor = Cursors.Hand
         PictureBox11.Dock = DockStyle.Fill
         PictureBox11.Image = My.Resources.Resources.Carte_pokemon_dos
         PictureBox11.Location = New Point(463, 428)
         PictureBox11.Margin = New Padding(3, 4, 3, 4)
         PictureBox11.Name = "PictureBox11"
-        PictureBox11.Size = New Size(221, 201)
+        PictureBox11.Size = New Size(224, 204)
         PictureBox11.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox11.TabIndex = 10
         PictureBox11.TabStop = False
         ' 
         ' PictureBox12
         ' 
+        PictureBox12.BackColor = Color.Transparent
         PictureBox12.BackgroundImageLayout = ImageLayout.None
+        PictureBox12.Cursor = Cursors.Hand
         PictureBox12.Dock = DockStyle.Fill
         PictureBox12.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox12.Location = New Point(692, 428)
+        PictureBox12.Location = New Point(693, 428)
         PictureBox12.Margin = New Padding(3, 4, 3, 4)
         PictureBox12.Name = "PictureBox12"
-        PictureBox12.Size = New Size(223, 201)
+        PictureBox12.Size = New Size(224, 204)
         PictureBox12.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox12.TabIndex = 11
         PictureBox12.TabStop = False
         ' 
         ' PictureBox13
         ' 
+        PictureBox13.BackColor = Color.Transparent
         PictureBox13.BackgroundImageLayout = ImageLayout.None
+        PictureBox13.Cursor = Cursors.Hand
         PictureBox13.Dock = DockStyle.Fill
         PictureBox13.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox13.Location = New Point(5, 639)
+        PictureBox13.Location = New Point(3, 640)
         PictureBox13.Margin = New Padding(3, 4, 3, 4)
         PictureBox13.Name = "PictureBox13"
-        PictureBox13.Size = New Size(221, 201)
+        PictureBox13.Size = New Size(224, 204)
         PictureBox13.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox13.TabIndex = 12
         PictureBox13.TabStop = False
         ' 
         ' PictureBox14
         ' 
+        PictureBox14.BackColor = Color.Transparent
         PictureBox14.BackgroundImageLayout = ImageLayout.None
+        PictureBox14.Cursor = Cursors.Hand
         PictureBox14.Dock = DockStyle.Fill
         PictureBox14.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox14.Location = New Point(234, 639)
+        PictureBox14.Location = New Point(233, 640)
         PictureBox14.Margin = New Padding(3, 4, 3, 4)
         PictureBox14.Name = "PictureBox14"
-        PictureBox14.Size = New Size(221, 201)
+        PictureBox14.Size = New Size(224, 204)
         PictureBox14.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox14.TabIndex = 13
         PictureBox14.TabStop = False
         ' 
         ' PictureBox15
         ' 
+        PictureBox15.BackColor = Color.Transparent
         PictureBox15.BackgroundImageLayout = ImageLayout.None
+        PictureBox15.Cursor = Cursors.Hand
         PictureBox15.Dock = DockStyle.Fill
         PictureBox15.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox15.Location = New Point(463, 639)
+        PictureBox15.Location = New Point(463, 640)
         PictureBox15.Margin = New Padding(3, 4, 3, 4)
         PictureBox15.Name = "PictureBox15"
-        PictureBox15.Size = New Size(221, 201)
+        PictureBox15.Size = New Size(224, 204)
         PictureBox15.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox15.TabIndex = 14
         PictureBox15.TabStop = False
         ' 
         ' PictureBox16
         ' 
+        PictureBox16.BackColor = Color.Transparent
         PictureBox16.BackgroundImageLayout = ImageLayout.None
+        PictureBox16.Cursor = Cursors.Hand
         PictureBox16.Dock = DockStyle.Fill
         PictureBox16.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox16.Location = New Point(692, 639)
+        PictureBox16.Location = New Point(693, 640)
         PictureBox16.Margin = New Padding(3, 4, 3, 4)
         PictureBox16.Name = "PictureBox16"
-        PictureBox16.Size = New Size(223, 201)
+        PictureBox16.Size = New Size(224, 204)
         PictureBox16.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox16.TabIndex = 15
         PictureBox16.TabStop = False
         ' 
         ' PictureBox17
         ' 
+        PictureBox17.BackColor = Color.Transparent
         PictureBox17.BackgroundImageLayout = ImageLayout.None
+        PictureBox17.Cursor = Cursors.Hand
         PictureBox17.Dock = DockStyle.Fill
         PictureBox17.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox17.Location = New Point(5, 850)
+        PictureBox17.Location = New Point(3, 852)
         PictureBox17.Margin = New Padding(3, 4, 3, 4)
         PictureBox17.Name = "PictureBox17"
-        PictureBox17.Size = New Size(221, 204)
+        PictureBox17.Size = New Size(224, 204)
         PictureBox17.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox17.TabIndex = 16
         PictureBox17.TabStop = False
         ' 
         ' PictureBox18
         ' 
+        PictureBox18.BackColor = Color.Transparent
         PictureBox18.BackgroundImageLayout = ImageLayout.None
+        PictureBox18.Cursor = Cursors.Hand
         PictureBox18.Dock = DockStyle.Fill
         PictureBox18.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox18.Location = New Point(234, 850)
+        PictureBox18.Location = New Point(233, 852)
         PictureBox18.Margin = New Padding(3, 4, 3, 4)
         PictureBox18.Name = "PictureBox18"
-        PictureBox18.Size = New Size(221, 204)
+        PictureBox18.Size = New Size(224, 204)
         PictureBox18.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox18.TabIndex = 17
         PictureBox18.TabStop = False
         ' 
         ' PictureBox19
         ' 
+        PictureBox19.BackColor = Color.Transparent
         PictureBox19.BackgroundImageLayout = ImageLayout.None
+        PictureBox19.Cursor = Cursors.Hand
         PictureBox19.Dock = DockStyle.Fill
         PictureBox19.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox19.Location = New Point(463, 850)
+        PictureBox19.Location = New Point(463, 852)
         PictureBox19.Margin = New Padding(3, 4, 3, 4)
         PictureBox19.Name = "PictureBox19"
-        PictureBox19.Size = New Size(221, 204)
+        PictureBox19.Size = New Size(224, 204)
         PictureBox19.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox19.TabIndex = 18
         PictureBox19.TabStop = False
         ' 
         ' PictureBox20
         ' 
+        PictureBox20.BackColor = Color.Transparent
         PictureBox20.BackgroundImageLayout = ImageLayout.None
+        PictureBox20.Cursor = Cursors.Hand
         PictureBox20.Dock = DockStyle.Fill
         PictureBox20.Image = My.Resources.Resources.Carte_pokemon_dos
-        PictureBox20.Location = New Point(692, 850)
+        PictureBox20.Location = New Point(693, 852)
         PictureBox20.Margin = New Padding(3, 4, 3, 4)
         PictureBox20.Name = "PictureBox20"
-        PictureBox20.Size = New Size(223, 204)
+        PictureBox20.Size = New Size(224, 204)
         PictureBox20.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox20.TabIndex = 19
         PictureBox20.TabStop = False
         ' 
         ' TableLayoutPanel1
         ' 
-        TableLayoutPanel1.BackColor = SystemColors.ButtonFace
-        TableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset
+        TableLayoutPanel1.BackColor = Color.Transparent
         TableLayoutPanel1.ColumnCount = 4
         TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25F))
         TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25F))
